3c95ead86a2cf5c24a2ddefdf72025d09d8c3ffd
Sutormin Evgeniy <sutormin.evgeniy@yandex.ru>
2016-12-11 23:50:35 +0300

    add-node

---
+++ shporanode.js
@@ -0,0 +1,28 @@
+// создание проекта (package.json)
+npm init
+
+/* npmjs.org */
+// установка модуля
+npm install --save express
+(npm install --save-dev express)
+// установка модулей в package.json
+npm install
+(npm i)
+// глобально
+npm install --g express
+
+// удаление модуля
+npm remove express
+
+// удаление не используемых модулей
+npm prune
+
+// обновление всех модулей
+npm update
+
+// запуск приложения
+node app
+node app.js
+
+// подсказка
+npm help prune
