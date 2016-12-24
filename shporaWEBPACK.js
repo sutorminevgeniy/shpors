8f0bfbaf94ce6e84ec23dfe2ffad4ea522dab035
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2016-12-25 02:54:35 +0300

    commit

--- shporaWEBPACK.js
+++ shporaWEBPACK.js
@@ -1,9 +1,17 @@
 // установка модулей
 // глобально
 npm install webpack -g
+npm install --g webpack
 
 // локально
 npm install webpack --save-dev
 
 
 webpack main.js bundle.js
+
+
+// простейший модуль статики
+// установка (глобально)
+npm install --g node-static
+// запуск
+static &
