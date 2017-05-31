6d37c36f9c17cf406493356ede9332b78d8c10e5
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2017-05-31 03:00:31 +0300

    commit

--- shporaReact.js
+++ shporaReact.js
@@ -1,7 +1,15 @@
 // настройка webpack
   npm init -y
 
+  npm uninstall react react-dom
   npm install --save react react-dom
+  npm install --save react-router@4.1.1
+  npm install --save react-router
+  npm uninstall react-router
+  npm install --save redux react-redux
+  npm install --save redux@3.3.1 react-redux@4.4.1
+  npm uninstall redux react-redux
+
   npm install --save-dev webpack webpack-dev-server
   npm install --save-dev babel-core babel-preset-es2015 babel-preset-react babel-loader
   npm install --save-dev css-loader style-loader
@@ -9,4 +17,6 @@
   npm install --save-dev react-hot-loader
 
   npm install --save-dev file-loader
-  npm install --save react-router
+
+npm uninstall react react-router react-dom redux react-redux
+npm install --save  react react-router react-dom redux react-redux
