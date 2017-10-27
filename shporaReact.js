dd604472dc74b094755511868ebd7ec8cf8d1ef7
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2017-10-27 18:12:30 +0300

    react

--- shporaReact.js
+++ shporaReact.js
@@ -1,22 +1,31 @@
 // настройка webpack
-  npm init -y
+npm init -y
 
-  npm uninstall react react-dom
-  npm install --save react react-dom
-  npm install --save react-router@4.1.1
-  npm install --save react-router
-  npm uninstall react-router
-  npm install --save redux react-redux
-  npm install --save redux@3.3.1 react-redux@4.4.1
-  npm uninstall redux react-redux
+npm install --save react react-dom
+npm install --save react-router
+npm install --save redux react-redux
 
-  npm install --save-dev webpack webpack-dev-server
-  npm install --save-dev babel-core babel-preset-es2015 babel-preset-react babel-loader
-  npm install --save-dev css-loader style-loader
-  npm install --save-dev node-sass sass-loader
-  npm install --save-dev react-hot-loader
+npm install --save-dev webpack webpack-dev-server
+npm install --save-dev babel-core babel-preset-env babel-loader babel-preset-react
+npm install --save-dev babel-plugin-transform-react-jsx-img-import
+npm install --save-dev file-loader
+npm install --save-dev css-loader style-loader
+npm install --save-dev node-sass sass-loader
+npm install --save-dev react-hot-loader
 
-  npm install --save-dev file-loader
+
 
 npm uninstall react react-router react-dom redux react-redux
 npm install --save  react react-router react-dom redux react-redux
+
+npm uninstall babel-preset-es2015
+npm install --save-dev babel-preset-env
+
+npm uninstall react react-dom
+npm install --save react react-dom
+npm install --save react-router@4.1.1
+npm install --save react-router
+npm uninstall react-router
+npm install --save redux react-redux
+npm install --save redux@3.3.1 react-redux@4.4.1
+npm uninstall redux react-redux
