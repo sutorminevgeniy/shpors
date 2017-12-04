eb570bfc4470bc6cc273197803b05545ece8b8cd
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2017-12-04 18:56:50 +0300

    commit

--- shporaReact.js
+++ shporaReact.js
@@ -3,7 +3,8 @@ npm init -y
 
 npm install --save react react-dom
 npm install --save redux react-redux
-npm install --save react-router react-router-redux
+npm install --save react-router-dom react-router-redux@next
+npm install --save history
 
 npm install --save-dev webpack webpack-dev-server
 npm install --save-dev babel-core babel-preset-env babel-loader babel-preset-react
@@ -15,26 +16,19 @@ npm install --save-dev html-webpack-plugin
 npm install --save-dev react-hot-loader
 
 
+
 npm install --save react-addons-css-transition-group
 
-
-npm uninstall react react-router react-dom redux react-redux
-npm install --save  react react-router react-dom redux react-redux
-
-npm uninstall babel-preset-es2015
-npm install --save-dev babel-preset-env
-
-npm uninstall react react-dom
 npm install --save react react-dom
 npm install --save react-router
-npm uninstall react-router
+npm install --save react-router@3
 npm install --save redux react-redux
-npm install --save redux@3.3.1 react-redux@4.4.1
-npm uninstall redux react-redux
-
-npm uninstall react-router-redux
-
 npm install --save react-router-redux@next
 npm install --save history
+
+npm uninstall redux react-redux
+npm uninstall react react-dom
+npm uninstall react-router
+npm uninstall react-router-redux
 
 npm install --g node-gyp@3.6.2
