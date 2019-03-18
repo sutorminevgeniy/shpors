21e1334e5cecf6ab473650d7d152157ede58e3d5
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2019-03-18 11:19:39 +0300

    commit

--- sporaADA.js
+++ sporaADA.js
@@ -8,3 +8,9 @@
   clip: rect(0, 0, 0, 0);
   border: 0;
 }
+
+
+<div aria-live="polite" role="log" aria-relevant="additions" aria-atomic="false" style="position: absolute; width: 1px; height: 1px; margin-top: -1px; clip: rect(1px, 1px, 1px, 1px); overflow: hidden;">
+  <div>The stimulus voltage was changed to 4.2 volts.</div>
+  <div>The stimulus voltage was changed to 4.3 volts.</div>
+</div>
