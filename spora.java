65031bf8926725bef9aaa4d4fb215c4deda0ab36
sutorminevgeniy <sutormin.evgeniy@yandex.ru>
2019-11-14 02:38:22 +0300

    add java

---
+++ spora.java
@@ -0,0 +1,33 @@
+System.out.print("Hello world! ");
+System.out.println("Note for Java");
+
+byte a = 5; // -128 127
+short b = -100; // -32768 32767
+int c;
+c = 1000000;
+long d = 1122334455;
+System.out.println();
+System.out.println(a);
+System.out.println("short b: " + b);
+
+double double1 = 133.155;
+System.out.println();
+System.out.println("double double1: " + double1);
+float float1 = 150.32f;
+System.out.println("float float1: " + float1);
+
+boolean boolean1 = true;
+boolean boolean2 = false;
+System.out.println();
+System.out.println(boolean1);
+System.out.println(boolean2);
+
+String str = "Hello!";
+System.out.println();
+System.out.println("String str: " + str);
+
+char char1 = 'w';
+System.out.println();
+System.out.println("char char1: " + char1);
+
+System.out.println("Good bye!");
